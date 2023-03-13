b6757434e445851943eb2a76451592299927cb6d
votriet <92041076+votriet@users.noreply.github.com>
2023-03-13 22:09:07 +0700

    Update Chitietsp.js

--- src/pages/Chitietsp/Chitietsp.js
+++ src/pages/Chitietsp/Chitietsp.js
@@ -2,7 +2,7 @@ import React from 'react'
 import classNames from 'classnames/bind';
 import styles from './Chitietsp.module.scss';
 const cx = classNames.bind(styles);
-export default function chitietsp() {
+export default function Chitietsp() {
   return (
     <div className={cx('chitiet')}>
      sdsadsads
