8b93136ef8cbb4e32b8d6b1e0f4a27c2fa2bb920
votriet <92041076+votriet@users.noreply.github.com>
2023-03-15 23:45:02 +0700

    update trang detail

--- src/pages/ProductDetails/ProductDetails.js
+++ src/pages/ProductDetails/ProductDetails.js
@@ -26,26 +26,64 @@ const ProductDetails = ()=>{
 getProducts();
 }, []
   );
+  const Loading = () => {
+    return(
+      <>
+      loading....
+      </>
+    )
+  }
+  const ShowProducts = () => {
+    return (
+    <div className='buttons d-flex justify-content-center mb-5 pb-5'>
+      <button className='btn btn-outline-dark me-2'>All</button>
+    <button className='btn btn-outline-dark'>Men's Clothing</button>
+    <button className='btn btn-outline-dark'>Women's Clothing</button>
+    <button className='btn btn-outline-dark'>Jewelery</button>
+    <button className='btn btn-outline-dark'>Electronic</button>
+    
+    </div>
+    {filter.map((ProductDetails) => {
+     
+    }
+    )
+    }
+
+    }
+
+
+  </div>
+)
+    }
+  }
+
+
+
+
   return (
     <div className='container'>
       <div className='row'>
         <div className='col-12 mb-5'>
           <h1>product</h1>
+
           <hr />
         </div>
+      </div>
+      <div className='row justifyContent: 'center>
+        {loading ? <loading/>  :<ProductDetails/> }
       </div>
     </div>
   );
 }
-export default function ProductDetails() {
-  return (
-    <div className={cx('chitiet')}>
-     <h1 className='heading'>Danh sách sản phẩm</h1>
+// export default function ProductDetails() {
+//   return (
+//     <div className={cx('chitiet')}>
+//      <h1 className='heading'>Danh sách sản phẩm</h1>
       
      
-     </div>
+//      </div>
     
 
-  )
+//   )
   
-}
+// }
