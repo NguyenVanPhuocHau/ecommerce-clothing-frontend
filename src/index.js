3adccf7390be02972487d68ee5ee7e0d01adc8bf
NguyenVanPhuocHau-19130066 <92570580+NguyenVanPhuocHau-19130066@users.noreply.github.com>
2023-04-20 20:50:22 +0700

    complate login register

--- src/index.js
+++ src/index.js
@@ -3,24 +3,28 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
-
+import GlobalStyle from './components/GlobalStyles';
+import './assets/css/responsive.css';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistStore } from 'redux-persist';
+import { SearchContextProvider } from './context/SearchContext';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(
-  <React.StrictMode>
-    <App/>
-  </React.StrictMode>
-);
+let persistor = persistStore(store);
 // import 'bootstrap/dist/css/bootstrap.min.css';
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+root.render(
+    <Provider store={store}>
+        <PersistGate persistor={persistor}>
+            <SearchContextProvider>
+                {/* <React.StrictMode> */}
+                <App />
+                {/* </React.StrictMode>, */}
+            </SearchContextProvider>
+        </PersistGate>
+    </Provider>,
+    document.getElementById('root'),
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
